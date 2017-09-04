bd12f2101d1dca1cbcbc3bbaca7c16ae2c60675e
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-09-04 09:15:38 -0700

    general gameplay completed

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -5,13 +5,82 @@ YTK.hangman = (function() {
   var prefix = 'YTK_',
       wordsArray = [
       "rhaegal",
+      "saitama",
       "covfefe",
+      "zzyzx road",
       "evangelion",
       "brendan eich",
       "trinity force",
       "mikasa ackerman",
-      ];
+      ],
+      avatars = {
+        "correct": [
+          " happy-1",
+          " happy-2",
+          " happy-3"
+        ],
+        "incorrect": [
+          " sad-1",
+          " sad-2",
+          " sad-3"
+        ]
+      },
+      bethSpeech = {
+        "start": [
+          "Sweet, cat paws!",
+          "Yay I <3 cats",
+          "Ohh Meow-nificen"
+        ],
+        "correct": [
+          "Ha! I'm too good",
+          "GG Noobz! Too easy",
+          "LOLz 1st grade level",
+          "Noice~"
+        ],
+        "incorrect": [
+          "I literally can't even",
+          "Could you flipping not?",
+          "Nope",
+          "Weeeak"
+        ],
+        "win" : [
+          "Reminds me of my spelling bee years!"
+        ],
+        "lose" : [
+          "This ain't over yet, not by a long shot!"
+        ]
+      },
+      larSpeech = {
+        "start": [
+          "$ is power my friend.",
+          "Can always use more $",
+          "$ is no jQuery here"
+        ],
+        "correct": [
+          "Power of the VIP",
+          "Good right? My idea!",
+          "My IQ is one of the highest",
+          "Now we talking~"
+        ],
+        "incorrect": [
+          "That's low",
+          "Unfair! Calling my lawyer",
+          "Worst deal EVER",
+          "Impossibru!"
+        ],
+        "win" : [
+          "Reminds me of my spelling bee years!"
+        ],
+        "lose" : [
+          "This ain't over yet, not by a long shot!"
+        ]
+      },
+      alphabets;
 
+  function getAlphabetArr() {
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return str.toLowerCase().split('');
+  }
   function ensureStorage() {
     return typeof(Storage) !== "undefined";
   }
@@ -52,14 +121,107 @@ YTK.hangman = (function() {
     var answer = getAnswer();
 
     localStorage.setItem(prefix + 'charID', charID);
-    localStorage.setItem(prefix + 'tries', 5);
+    localStorage.setItem(prefix + 'hearts', 5);
     localStorage.setItem(prefix + 'skillOne', 1);
     localStorage.setItem(prefix + 'skillTwo', 1);
     localStorage.setItem(prefix + 'answer', answer);
     localStorage.setItem(prefix + 'unique', JSON.stringify(getUniqueCharArray(answer)));
   }
 
+  function buildDashes(answerStr){
+    var wordsArr = answerStr.split(' '),
+        hangmanMain = document.getElementById('hangman-main'),
+        charID = parseInt(localStorage.getItem(prefix + 'charID')),
+        charArr, rowDiv, dashDiv;
+
+    for(var i = 0; i < wordsArr.length; i++) {
+      rowDiv = document.createElement("div");
+      rowDiv.className = 'dash-row';
+
+      charArr = wordsArr[i].split('');
+
+      for(var j = 0; j < charArr.length; j++) {
+        dashDiv = document.createElement("div");
+        dashDiv.className = 'dash char-' + alphabets.indexOf(charArr[j]);
+        dashDiv.className += (charID ? '' : ' lar');
+        rowDiv.appendChild(dashDiv);
+      }
+
+      hangmanMain.appendChild(rowDiv);
+    }
+  }
+
+  function guessCorrect(dashDivs) {
+    return dashDivs.length > 0;
+  }
+  function getCharDivsByID(alphabetID) {
+    return document.getElementsByClassName('char-' + alphabetID);
+  }
+  function revealDashs(alphabetDivs, alphabetID){
+    for (var i = 0; i < alphabetDivs.length; i++) {
+      alphabetDivs[i].className += ' reveal';
+      alphabetDivs[i].innerHTML = alphabets[alphabetID].toUpperCase();
+    }
+  }
+  function loseHeart(heartsTot) {
+    document.getElementById('heart-' + heartsTot).style.color = 'transparent';
+    localStorage.setItem(prefix + 'hearts', (heartsTot - 1));
+  }
+  function incorrectActions() {
+    var heartsTotal = parseInt(localStorage.getItem(prefix + 'hearts'));
+
+    loseHeart(heartsTotal);
+
+    if (heartsTotal === 1) {
+      $('#endGameModal').modal('show')
+    }
+  }
+
+  function updateSpeech(charID, action) {
+    var speechArr = (charID ? bethSpeech : larSpeech),
+        rand = getRandomInt(0, speechArr[action].length -1);
+
+    document.getElementById("main-speech").innerHTML = speechArr[action][rand];
+  }
+
+  function updateAvatar(charID, action){
+    var avatarDiv = charID ? document.getElementById('beth-avatar') : document.getElementById('lar-avatar'),
+        rand = getRandomInt(0, avatars[action].length -1);
+        
+    avatarDiv.className = 'avatar' + avatars[action][rand];
+  }
+
+  function hideCharContent(charID) {
+    var hideChar = charID ? 'lar' : 'beth',
+        hideDivs = document.getElementsByClassName(hideChar);
+
+    for (var i = 0; i < hideDivs.length; i++) {
+      hideDivs[i].className += ' hidden';
+    }  
+  }
+
+  function pickChar(e, alphabetID) {
+    var dashDivs = getCharDivsByID(alphabetID),
+        charID = parseInt(localStorage.getItem(prefix + 'charID'));
+
+    e.setAttribute('disabled', true);
+
+    if (guessCorrect(dashDivs)) {
+      updateSpeech(charID, 'correct');
+      updateAvatar(charID, 'correct');
+      revealDashs(dashDivs, alphabetID);
+    }
+
+    else {
+      updateSpeech(charID, 'incorrect');
+      updateAvatar(charID, 'incorrect');
+      incorrectActions();
+    }
+  }
+
   function initGame(charID) {
+
+    alphabets = getAlphabetArr();
 
     if (!ensureStorage() || !ensureCharID(charID)) {
       console.log('something went wrong on game start!');
@@ -68,12 +230,24 @@ YTK.hangman = (function() {
 
     initStorage(charID);
 
+    hideCharContent(charID ? 1 : 0);
+
+    buildDashes(localStorage.getItem(prefix + 'answer'));
+
+    updateSpeech(charID, 'start');
+
     console.log('localstorage: ', localStorage);
   }
 
-
   return {
-    initGame: initGame
+    initGame: initGame,
+    pickChar: pickChar
   }
+
 })();
+
+jQuery(window).on('load', function() {
+  YTK.hangman.initGame(0);
+})
+
 
